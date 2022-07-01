f4cf2feb15a239ad5701a6a3253905a7cb9cfe4e
Pablo Moreno <ssm.pablo@gmail.com>
2022-07-01 17:07:43 +0200

    basic routing logic and pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { Index } from './pages/Index';
+import { About } from './pages/About';
 
-function App() {
+const App: React.FC = () => {
   return (
-  <div>helo</div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Route path="/" element={<Index />} />
+        <Route path="/about" element={<About />} />
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/Index.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Index: React.FC = () => {
+  return <h2>Index</h2>;
+}
